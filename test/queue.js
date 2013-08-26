7206fcd9ba1df34720b0ee7e8660b1f3fe246677
Will White <will@mapbox.com>
2013-08-26 14:48:28 -0400

    Add basic tests.

---
+++ test/queue.js
@@ -0,0 +1,29 @@
+var Queue = require('../index.js');
+var assert = require("assert")
+
+describe('queue', function() {
+    describe('add', function(done) {
+        it('should increment running up to concurrency', function() {
+            var q = new Queue(function() {}, 3);
+            assert.equal(q.running, 0);
+            q.add('one');
+            assert.equal(q.running, 1);
+            q.add('two');
+            assert.equal(q.running, 2);
+            q.add('three');
+            assert.equal(q.running, 3);
+            q.add('four');
+            assert.equal(q.running, 3);
+        });
+    });
+    describe('on empty', function() {
+        it('should be invoked when queue is empty', function(done) {
+            var q = new Queue(function(obj, callback) { callback() }, 3);
+            q.on('empty', function() {
+                assert.equal(q.running, 0);
+                done();
+            });
+            q.add('foo');
+        });
+    });
+});
